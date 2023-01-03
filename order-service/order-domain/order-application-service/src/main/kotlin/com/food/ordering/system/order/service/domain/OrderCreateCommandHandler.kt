841707694cbb99504647801a3885f56316a33c24
Omprakash Sridharan <omprakash.sridharan@sumup.com>
2023-01-03 16:49:05 +0100

    Adds command handlers

--- order-service/order-domain/order-application-service/src/main/kotlin/com/food/ordering/system/order/service/domain/OrderCreateCommandHandler.kt
+++ order-service/order-domain/order-application-service/src/main/kotlin/com/food/ordering/system/order/service/domain/OrderCreateCommandHandler.kt
@@ -1,67 +1,26 @@
 package com.food.ordering.system.order.service.domain
 
-import com.food.ordering.system.domain.exception.OrderDomainException
 import com.food.ordering.system.order.service.domain.dto.create.CreateOrderCommand
 import com.food.ordering.system.order.service.domain.dto.create.CreateOrderResponse
-import com.food.ordering.system.order.service.domain.entity.Order
-import com.food.ordering.system.order.service.domain.entity.Restaurant
 import com.food.ordering.system.order.service.domain.mapper.OrderDataMapper
-import com.food.ordering.system.order.service.domain.ports.output.repository.CustomerRepository
-import com.food.ordering.system.order.service.domain.ports.output.repository.OrderRepository
-import com.food.ordering.system.order.service.domain.ports.output.repository.RestaurantRepository
+import com.food.ordering.system.order.service.domain.ports.output.message.publisher.payment.OrderCreatedPaymentRequestMessagePublisher
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
-import org.springframework.transaction.annotation.Transactional
-import java.util.*
 
 @Component
 class OrderCreateCommandHandler(
     @Autowired
-    val orderDomainService: OrderDomainService,
+    val orderCreateHelper: OrderCreateHelper,
     @Autowired
-    val orderRepository: OrderRepository,
+    val orderDataMapper: OrderDataMapper,
     @Autowired
-    val customerRepository: CustomerRepository,
-    @Autowired
-    val restaurantRepository: RestaurantRepository,
-    @Autowired
-    val orderDataMapper: OrderDataMapper
+    val orderCreatedPaymentRequestMessagePublisher: OrderCreatedPaymentRequestMessagePublisher
 ) {
 
-    @Transactional
     fun createOrder(createOrderCommand: CreateOrderCommand): CreateOrderResponse {
-        checkCustomer(createOrderCommand.customerId)
-        val restaurant = checkRestaurant(createOrderCommand);
-        val order = orderDataMapper.createOrderCommandToOrder(createOrderCommand)
-        val orderCreatedEvent = orderDomainService.validateAndInitiateOrder(order, restaurant)
-        val orderSaveResult = saveOrder(order)
-        return orderDataMapper.orderToCreateOrderResponse(orderSaveResult)
+        val orderCreatedEvent = orderCreateHelper.persistOrder(createOrderCommand)
+        orderCreatedPaymentRequestMessagePublisher.publish(orderCreatedEvent)
+        return orderDataMapper.orderToCreateOrderResponse(orderCreatedEvent.order)
     }
 
-    private fun saveOrder(order: Order): Order {
-        val orderResult = orderRepository.save(order)
-        if (orderResult.isEmpty) {
-            throw OrderDomainException("Could not save order");
-        }
-        return orderResult.get()
-    }
-
-    private fun checkRestaurant(createOrderCommand: CreateOrderCommand): Restaurant {
-        val restaurant = orderDataMapper.createOrderCommandToRestaurant(createOrderCommand)
-        val optionalRestaurant = restaurantRepository.findRestaurantInfo(restaurant)
-        if (optionalRestaurant.isEmpty) {
-            throw OrderDomainException(
-                "Could not find restaurant with restaurant id ${createOrderCommand.restaurantId}",
-                null
-            )
-        }
-        return optionalRestaurant.get()
-    }
-
-    private fun checkCustomer(customerId: UUID) {
-        val optionalCustomer = customerRepository.findCustomer(customerId)
-        if (optionalCustomer.isEmpty) {
-            throw OrderDomainException("Could not find customer with customer id $customerId", null)
-        }
-    }
 }
